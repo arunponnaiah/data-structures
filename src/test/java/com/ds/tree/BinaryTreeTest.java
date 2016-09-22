84149429e5ea5f47f1a98fed2805e0ea9dc1fb3d
Arun Ponnaiah <arun.ponnaiah@gmail.com>
2016-09-21 22:58:14 -0700

    Added junit test for BinaryTree

---
+++ src/test/java/com/ds/tree/BinaryTreeTest.java
@@ -0,0 +1,22 @@
+package com.ds.tree;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BinaryTreeTest {
+	private BinaryTree binaryTree;
+
+	@Before
+	public void setUp() throws Exception {
+		this.binaryTree = new BinaryTree(10);
+	}
+
+	@Test
+	public void testAddNewNode() {
+		this.binaryTree.add(5);
+		this.binaryTree.add(7);
+		this.binaryTree.add(15);
+		this.binaryTree.add(17);
+	}
+
+}
